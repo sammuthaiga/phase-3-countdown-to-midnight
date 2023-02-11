50692d0c2570027071b666d08583b6f83391ee5f
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-12 01:03:17 +0300

    commit tree a7662e12d99a67373720425c1d3989a7d1c8d207

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
-
-def countdown
-end
+def countdown(seconds)
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
+  def countdown_with_sleep(seconds)
+    while seconds > 0
+      puts "#{seconds} SECOND(S)!"
+      sleep(1)
+      seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+  end
+  
